b44a6bb3577629d2cedf58fab909b6b9ba129622
Errant <eltehhh@gmail.com>
2025-08-13 21:02:49 +0800

    tags

--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -4,9 +4,11 @@ import { Link } from "react-router-dom"
 
 
 
+
 export default function Gallery() {
 
     const [portfolioJson, setPortfolioJson] = useState({})
+    const [portfolioTags, setPortfolioTags] = useState({})
     const numGalleryCols = 3
 
 
@@ -15,13 +17,27 @@ export default function Gallery() {
             setPortfolioJson(data)
 
         })
+        fetch('assets/portfoliotags.json').then((res) => res.json()).then((data) => {
+            setPortfolioTags(data)
 
-
+        })
 
     }, [])
 
     let jsonRange = [...Array(Math.floor(Object.keys(portfolioJson).length / numGalleryCols)).keys()]
 
+    const GalleryImage = ({filename}) => {
+
+        return <div>
+            <img src={"assets/pics/" + filename}
+        className={"object-cover h-64"}
+        key={filename} />
+        {portfolioJson[filename].tags.map((tag) => {
+            return portfolioTags[tag].fullName
+        })}
+        </div>
+
+    }
 
 
     return (
@@ -29,13 +45,11 @@ export default function Gallery() {
             <h1 className="text-2xl font-bold">Gallery</h1>
             <div>pics go here</div>
             <div>back to <Link to="/"><u>home</u></Link></div>
-            <div className="flex flex-row justify-evenly flex-wrap w-4/5 p-5">
+            <div className="flex flex-row justify-evenly flex-wrap w-4/5 p-5 gap-y-2">
                 {
                     Object.keys(portfolioJson).map((filename) => {
                                 console.log(filename)
-                                return <div className = " p-1"><img src={"assets/pics/" + filename}
-                                    className={"object-cover h-64"}
-                                    key={filename} />
+                                return <div className = " p-1"><GalleryImage filename = {filename}/>
                                     </div>
 
                             })
