5448addf91f36c746b4919f2151cd27012f69ea3
Errant <eltehhh@gmail.com>
2025-08-13 15:50:48 +0800

    stuff

--- src/pages/dancingsnek.js
+++ src/pages/dancingsnek.js
@@ -4,7 +4,7 @@ export default function DancingSnek() {
     return (
         <div className="w-screen h-screen flex items-center justify-center">
           <div className = "flex flex-row items-center justify-center">
-          <img src = "/assets/gangnamsnake.gif"/>
+          <img src = "/assets/gangnamsnake.webp"/>
           <div>
           <p>welome 2 hecking snecc</p>
           <p> he dance for u </p>
