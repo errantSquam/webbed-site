5e745af7bccd1152901b8eafd15d98178dd22314
Errant <eltehhh@gmail.com>
2025-08-13 20:23:46 +0800

    layout

--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -7,27 +7,63 @@ import { Link } from "react-router-dom"
 export default function Gallery() {
 
     const [portfolioJson, setPortfolioJson] = useState({})
+    const numGalleryCols = 3
 
-    useEffect(()=>{
-    fetch('assets/portfolio.json').then((res)=>res.json()).then((data)=>{
-      setPortfolioJson(data)
-     })
 
-     
-  },[])
+    useEffect(() => {
+        fetch('assets/portfolio.json').then((res) => res.json()).then((data) => {
+            setPortfolioJson(data)
+
+        })
+
+
+
+    }, [])
+
+    let jsonRange = [...Array(Math.floor(Object.keys(portfolioJson).length / numGalleryCols)).keys()]
+
+
+
     return (
-    <div className = "py-10 px-10 flex flex-col items-center text-center">
-        <h1 className = "text-2xl font-bold">Gallery</h1>
-        <div>pics go here</div>
-        <div>back to <Link to = "/"><u>home</u></Link></div>
-        {
-            Object.keys(portfolioJson).map((filename) => {return <div>
+        <div className="py-10 px-10 flex flex-col items-center text-center">
+            <h1 className="text-2xl font-bold">Gallery</h1>
+            <div>pics go here</div>
+            <div>back to <Link to="/"><u>home</u></Link></div>
+            <div className="flex flex-row justify-evenly flex-wrap w-4/5 p-5">
+                {
+                    Object.keys(portfolioJson).map((filename) => {
+                                console.log(filename)
+                                return <div className = " p-1"><img src={"assets/pics/" + filename}
+                                    className={"object-cover h-64"}
+                                    key={filename} />
+                                    </div>
 
-                <img src = {"assets/pics/" + filename}/>
-                </div>})
-        }
-    </div>
+                            })
+                 }
+                
+            </div>
+        </div>
 
     )
 
 }
+
+
+{/*<div className="flex flex-col px-10 w-2/3">
+                {jsonRange.map((index) => {
+                    return <div className="flex flex-row" key={index}>
+                        {
+                            [...Array(numGalleryCols).keys()].map((filenameIndex) => {
+                                let filename = Object.keys(portfolioJson)[(index + 1) * numGalleryCols + filenameIndex - 1]
+                                console.log(filename)
+                                return <img src={"assets/pics/" + filename}
+                                    className={"object-contain grow h-64"}
+                                    key={filenameIndex} />
+
+                            })
+                        }
+                    </div>
+                })
+                }
+            </div>
+*/}
